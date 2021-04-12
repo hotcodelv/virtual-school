91947952f3391f1b45240eaf2b29190eb46b025d
kristaps <kristaps.vitolins@gmail.com>
2021-04-12 17:22:56 +0300

    reduce size of planet

--- src/planet.js
+++ src/planet.js
@@ -22,7 +22,7 @@ export class Planet {
     addSunLinghtTo(scene);
 
     const texture = loader.load(this.texture);
-    const geometry = new THREE.SphereGeometry(3, 50, 50);
+    const geometry = new THREE.SphereGeometry(0.5, 50, 50);
     const meterial = new THREE.MeshPhongMaterial({
       map: texture,
     });
