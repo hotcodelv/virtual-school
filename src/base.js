004dc08c711a18b8fb00dc281320650e500d9f46
kristaps <kristaps.vitolins@gmail.com>
2021-04-13 08:07:21 +0300

    remove unused code and set 0 cords for sun

--- src/base.js
+++ src/base.js
@@ -1,8 +1,7 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 var camera = new THREE.PerspectiveCamera(145, window.innerWidth / window.innerHeight, 0.1, 180000);
-camera.position.set(0, 0, 0.01);
+camera.position.set(0, 0, 0);
 
 var scene = new THREE.Scene();
 scene.add(camera);
@@ -12,9 +11,6 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.domElement.style.position = "absolute";
 renderer.domElement.style.top = "0px";
 renderer.domElement.style.left = "0px";
-
-// var controls = new OrbitControls(camera, renderer.domElement);
-// controls.update();
 
 const secondsInDay = 87600;
 const distanceScale = 0.00000004;
