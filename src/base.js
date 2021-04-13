cd818ce41b0eced602c82cfe30e8e5af3ddd3ca9
kristaps <kristaps.vitolins@gmail.com>
2021-04-13 10:25:16 +0300

    revert orbit controls

--- src/base.js
+++ src/base.js
@@ -1,7 +1,8 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 var camera = new THREE.PerspectiveCamera(145, window.innerWidth / window.innerHeight, 0.1, 180000);
-camera.position.set(0, 0, 0);
+camera.position.set(0, 0, 0.01);
 
 var scene = new THREE.Scene();
 scene.add(camera);
@@ -12,9 +13,12 @@ renderer.domElement.style.position = "absolute";
 renderer.domElement.style.top = "0px";
 renderer.domElement.style.left = "0px";
 
+var controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
 const secondsInDay = 87600;
 const distanceScale = 0.00000004;
 const diameterScale = 18000;
 const timeScale = 1000000;
 
-export { camera, scene, renderer, secondsInDay, distanceScale, diameterScale, timeScale };
+export { camera, scene, renderer, controls, secondsInDay, distanceScale, diameterScale, timeScale };
